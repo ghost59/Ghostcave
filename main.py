89a813b251403ced9f310e4ffc48801d8c88c37b
ghost59 <as.1714554@gmail.com>
2022-11-07 22:22:08 -0500

    Changes set word
    Allows for the user to change the word hello world to anything. Also change the color and reverse the word.

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
-# Greeting
-def greeting():
-    name = input("Welcome to space wave studios, May I have your name?")
-    print(f"Thank you, {name}")
+import tkinter as tk
+from tkinter import ttk
 
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('350x200')
+        self.root.title('Text App')
+        self.mainframe = tk.Frame(self.root,background='white')
+        self.mainframe.pack(fill='both', expand=True)
 
-# Ask the user to put an pick from the following list.
+        self.text = ttk.Label(self.mainframe, text='Hello World', background='white', font=('Brass Mon', 30))
+    
+        self.text.grid(row=0, column=0)
+        self.set_text_field = ttk.Entry(self.mainframe)
+        self.set_text_field.grid(row=1, column=0, pady=10, sticky='NWES')
+        set_text_button = ttk.Button(self.mainframe, text='Set Text',command=self.set_text)
+        set_text_button.grid(row=1,column=1, pady=10)
+        
+        color_options = ['red','blue','green','black','grey','dark blue']
+        self.set_color_field = ttk.Combobox(self.mainframe, values=color_options )
+        self.set_color_field.grid(row=2, column=0, sticky='NWES', pady=10)
+        set_color_button = ttk.Button(self.mainframe, text='Set Color',command=self.set_color)
+        set_color_button.grid(row=2,column=1, pady=10)
 
-def select():
-    selction = input("Please pick from the following lists please "
-                     "[1]Ice cream, [2]Fire power, [3]Tools of chaos")
-    if selction == '1':
-        ice_cream()
+        self.reverse_text = ttk.Button(self.mainframe,text='Reverse Text', command=self.reverse)
+
+        self.reverse_text.grid(row=3, column=0, pady=10)
+        self.root.mainloop()
+        
         return
-    if selction == '2':
-        fire_power()
-        return
-    if selction == '3':
-        tools_of_chaos()
-        return
+    def set_text(self):
+        newtext = self.set_text_field.get()
+        self.text.config(text=newtext)
+    def set_color(self):
+        newcolor = self.set_color_field.get()
+        self.text.config(foreground=newcolor)
 
+    def reverse(self):
+        newtext = self.text.cget('text')
+        reversed = newtext[::-1]
+        self.text.config(text=reversed) 
 
-def ice_cream():
-    cream = input('Please select the type of ice cream')
-    print(cream)
-    return
-
-def fire_power():
-    power = ("We have the latest fire power in the world.")
-    print(power)
-    return
-
-def tools_of_chaos():
-    chaos = ("We have the latest tools of chaos ")
-    print(chaos)
-    return
-
-greeting()
-select()
+if __name__ == '__main__':
+    App()
